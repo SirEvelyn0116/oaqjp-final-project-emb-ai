ef42b45096da35fe625e29b06cc0934e18eac89b
Laurence Evelyn <adrian_laurence@hotmail.com>
2025-06-17 16:30:32 -0400

    added formatting of output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,14 +1,17 @@
 import requests
+import json
 
-def emotion_detector(text_to_analyze) -> str:
+def emotion_detector(text_to_analyze) -> {}:
     """
-    Use Watson NLP library to process inputted text and respond with it's response
+    Use Watson NLP library to process inputted text and return it's response
+    in JSON format
 
     Parameters:
     text_to_analyze (str): Text to pass to Watson NLP to analyze
 
     Returns:
-    str: The text attribute of the response object received from Watson NLP
+    {}: dictionary with required emotions and a determined dominant emotion parsed from
+        the response object received from Watson NLP
     """
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     data = { "raw_document": { "text": text_to_analyze }}
@@ -17,7 +20,24 @@ def emotion_detector(text_to_analyze) -> str:
     try:
         response = requests.post(url, json=data, headers=headers)
         response.raise_for_status()  # Raise an error for bad status codes
+        json_data = response.json()  # Convert response text to json dictionary
 
-        return response.text
+        required_emotions_keys = [
+            'anger', 
+            'disgust',
+            'fear',
+            'joy',
+            'sadness'
+        ]
+
+        required_emotions = {}
+
+        for key in required_emotions_keys:
+            required_emotions[key] = json_data.get("emotionPredictions", {})[0].get("emotion", {}).get(key)
+
+        required_emotions['dominant_emotion'] = max(required_emotions, key=required_emotions.get)
+
+        return print(json.dumps(required_emotions, indent=4))
+        
     except requests.exceptions.RequestException as returnedException:
         return f"An error occurred: {returnedException}"
